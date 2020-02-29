bc6516533cbcdcd3f3f15a0e702c62f371d4bede
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2020-02-29 15:13:00 +0530

    Updates

--- src/slugify.ts
+++
@@ -1,9 +0,0 @@
-import { Slugger } from 'marked'
-
-const cachedSlugger = new Slugger()
-
-function slugify(text: string, unique = false) {
-   return unique ? cachedSlugger.slug(text) : new Slugger().slug(text)
-}
-
-export default slugify
